d05bef9d593b3ccb9dbbea641a609d6021082d7b
Stephane Nouvellon <stephane@cloudflare.com>
2019-12-31 11:56:40 +0100

    Initial commit

---
+++ graphql.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+
+	"github.com/machinebox/graphql"
+)
+
+type respDataStruct struct {
+	Viewer Viewer `json:"viewer"`
+}
+type Dimensions struct {
+	CacheStatus string `json:"cacheStatus"`
+}
+type Sum struct {
+	EdgeResponseBytes int `json:"edgeResponseBytes"`
+}
+type HTTPRequestsCacheGroups struct {
+	Dimensions Dimensions `json:"dimensions"`
+	Sum        Sum        `json:"sum"`
+}
+type Zones struct {
+	HTTPRequestsCacheGroups []HTTPRequestsCacheGroups `json:"httpRequestsCacheGroups"`
+}
+type Viewer struct {
+	Zones []Zones `json:"zones"`
+}
+
+func buildGraphQLQuery(date string) *graphql.Request {
+
+	queryForCache := graphql.NewRequest(`
+	{
+		viewer {
+		  zones(filter: { zoneTag: $zoneTag }) {
+			httpRequestsCacheGroups(
+			  limit: 10000
+			  filter: { datetimeMinute_gt: $lastSuccessfulScrape }
+			) {
+			  dimensions {
+				cacheStatus
+			  }
+			  sum {
+				edgeResponseBytes
+			  }
+			}
+		  }
+		}
+	  }
+	  
+	`)
+
+	// set any variables
+	queryForCache.Var("zoneTag", "d88b6d7f404e420305cd6c9a73c60576")
+	queryForCache.Var("lastSuccessfulScrape", date)
+
+	return queryForCache
+}
+
+// Get cloudflare metrics from GraphQL using the provided api-email and api-key parameters and returns a marshalled JSON struct and an error if something went wrong during the fetch
+func getCloudflareCacheMetrics(query *graphql.Request, apiEmail string, apiKey string) (respData respDataStruct, err error) {
+	client := graphql.NewClient("https://api.cloudflare.com/client/v4/graphql")
+
+	req := query
+
+	// set header fields -> token and email!
+	req.Header.Set("x-auth-key", apiKey)
+	req.Header.Set("x-auth-email", apiEmail)
+	// define a Context for the request
+	ctx := context.Background()
+
+	// run it and capture the response
+
+	if err := client.Run(ctx, req, &respData); err != nil {
+		return respData, err
+	}
+	return respData, nil
+}
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/urfave/cli"
+)
+
+func recordMetrics(conf *config) func(c *cli.Context) error {
+	return func(c *cli.Context) error {
+		if err := parseFlags(conf, c); err != nil {
+			cli.ShowAppHelp(c)
+			log.Fatal(err)
+			return err
+
+		}
+		go func() {
+			var date = time.Now().Format(time.RFC3339)
+			date = "2019-12-27T11:55:21+01:00"
+			for {
+				time.Sleep(2 * time.Second)
+
+				resp, err := getCloudflareCacheMetrics(buildGraphQLQuery(date), conf.apiEmail, conf.apiKey)
+
+				if err == nil {
+					for _, node := range resp.Viewer.Zones[0].HTTPRequestsCacheGroups {
+						requestBytes.With(prometheus.Labels{"cacheStatus": node.Dimensions.CacheStatus}).Set(float64(node.Sum.EdgeResponseBytes))
+					}
+					fetchDone.Inc()
+				} else {
+					log.Println("Fetch failed: ", err)
+					fetchFailed.Inc()
+				}
+			}
+		}()
+		return nil
+	}
+}
+
+var (
+	fetchFailed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "cloudflare_failed_fetches",
+		Help: "The total number of failed fetches",
+	})
+	fetchDone = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "cloudflare_done_fetches",
+		Help: "The total number of done fetches",
+	})
+	requestBytes = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudflare_processed_bytes",
+		Help: "The total number of processed bytes, labelled per cache status",
+	},
+		[]string{"cacheStatus"},
+	)
+)
+
+func main() {
+	log.SetPrefix("[cloudflare-exporter] ")
+	log.SetFlags(log.Ltime)
+	log.SetOutput(os.Stderr)
+
+	app := cli.NewApp()
+	app.Name = "cloudflare-exporter"
+	app.Usage = "export Cloudflare metrics to prometheus"
+	app.Flags = flags
+
+	conf := &config{}
+	app.Action = recordMetrics(conf)
+	if err := app.Run(os.Args); err != nil {
+		log.Println(err)
+	}
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
+
+var flags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "api-key",
+		Usage: "Your Cloudflare API token",
+	},
+	cli.StringFlag{
+		Name:  "api-email",
+		Usage: "The email address associated with your Cloudflare API token and account",
+	},
+}
+
+type config struct {
+	apiKey   string
+	apiEmail string
+}
+
+func parseFlags(conf *config, c *cli.Context) error {
+	conf.apiKey = c.String("api-key")
+	conf.apiEmail = c.String("api-email")
+
+	return conf.Validate()
+}
+
+func (conf *config) Validate() error {
+
+	if conf.apiKey == "" || conf.apiEmail == "" {
+		return errors.New("Must provide both api-key and api-email")
+	}
+
+	return nil
+}
